b27def6a00be6bc6b299d8b1183c96f36cccf26a
bhavik103 <bhavikkalariya103@gmail.com>
2019-02-22 14:46:46 +0530

    gmailClone

--- gmailClone/js/myJs.js
+++ gmailClone/js/myJs.js
@@ -7,6 +7,7 @@ const topAppBarEl = document.querySelector('.mdc-top-app-bar');
 const topAppBar = new mdc.topAppBar.MDCTopAppBar.attachTo(topAppBarEl);
 
 topAppBar.setScrollTarget(document.querySelector('.main-content'));
+mdc.tabBar.MDCTabBar.attachTo(document.querySelector('.mdc-tab-bar'));
 topAppBar.listen('MDCTopAppBar:nav', () => {
   drawer.open = !drawer.open;
 });
